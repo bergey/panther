bf7786f40a39128f1c6a65b2e4fdf73f5f024064
Daniel Bergey <bergey@alum.mit.edu>
2015-02-26 18:40:11 +0000

    add tests for Mesh intersection

--- tests/UnitTests.hs
+++ tests/UnitTests.hs
@@ -8,6 +8,7 @@
 module Main where
 
 import Test.Tasty.QuickCheck
+import Test.Tasty.HUnit
 import Test.Tasty
 
 import Types -- Arbitrary instances
@@ -22,16 +23,35 @@ tests = testGroup "Panther Tests" [
     testGroup "sphere" [
          testProperty "intersect sphere at origin with radius 1" $
          \dir -> dir /= 0 ==>
-                 intersect (Sphere 0 1) (mkRay 0 (normalize dir) 0) ^?! _Wrapped . _Just . tHit =~ 1,
+                 intersect (Sphere 0 1) (mkRay 0 (normalize dir) 0) ^? _Wrapped . _Just . tHit =~ Just 1,
          testProperty "sphere has fixed r" $
          \(getSphere -> s@(Sphere c r), dir) -> dir /= 0 && r > 0 ==>
-                                   intersect s (mkRay c (normalize dir) 0) ^?! _Wrapped . _Just . tHit =~ r
+                                   intersect s (mkRay c (normalize dir) 0) ^? _Wrapped . _Just . tHit =~ Just r
          ],
     testGroup "plane" [
         testProperty "all rays intersect a plane" $
         \(getPlane -> plane, ray) -> isJust (getOption $ intersect (plane) ray)
                           || isJust (getOption $ intersect plane $ ray & rayDir *~ -1)
-                      ]
+                      ],
+    testGroup "mesh" [
+        testCase "a ray intersects a triangle" $ let
+             mesh = oneTriangleMesh $ V3 0 (p3 1 0 0) (p3 0 1 0)
+             ray = mkRay (p3 0.33 0.33 1) (V3 0 0 (-1)) 0
+        in intersect mesh ray ^? _Wrapped . _Just . tHit @?= Just 1,
+        testProperty "arbitrary direction, no false negatives, correct tHit" $
+        \(tri, bary, v) -> let
+            ray = mkRay o v 0
+            o = barycentric (firstTriangle tri) bary .-^ v
+            in intersect (getTri tri) ray ^? _Wrapped . _Just . tHit =~ Just 1,
+        testProperty "arbitrary direction, no false positives" $
+        \(tri, bary, v) -> let
+            ray = mkRay o v 0
+            o = barycentric (firstTriangle tri) bary .+^ v
+            in intersect (getTri tri) ray ^? _Wrapped . _Just . tHit =~ Nothing
+
+        -- testProperty "arbitrary rays, no false positives" $
+        -- \(tri, bary, v) -> let
+        ]
     ]
 
 
